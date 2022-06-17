d47b905a2c898bc41c8d10853adde99954f37cea
Hamza Aziane <hamza.aziane@pm.me>
2022-06-18 00:51:38 +0100

    Add playerSelection() function

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-let items = ['scissors', 'rock', 'paper'];
+const items = ['scissors', 'rock', 'paper'];
 
-function computerPlay() {
-    let min = 0, max = 2;
+function computerSelection() {
+    const min = 0, max = 2;
     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function playerSelection() {
+    const selection = prompt(`# Choose your weapon:
+    
+✂️ - Scissors
+🪨 - Rock
+📄 - Paper
+`);
+    return items.indexOf(selection.toLowerCase());
 }
 
